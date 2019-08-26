dae5ecbab4782551db7a1e40fb889b407bcff73b
Ilya <soloviev1097@gmail.com>
2019-08-26 12:31:00 +0300

    add realm

--- android/app/src/main/java/com/someproj/MainApplication.java
+++ android/app/src/main/java/com/someproj/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
+import io.realm.react.RealmReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,7 +24,8 @@ public class MainApplication extends NavigationApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RealmReactPackage()
       );
     }
   };
